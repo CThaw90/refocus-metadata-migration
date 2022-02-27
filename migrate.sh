0d75de98498940df6a8379720746b272041e0923
CThaw90 <christhaw90@gmail.com>
2022-02-27 01:09:20 -0500

    Added database name into the mysql script command

--- migrate.sh
+++ migrate.sh
@@ -6,7 +6,7 @@ fi
 while read SCRIPT_FILE; do
 	SCRIPT_FILE_PATH="$SCRIPT_HOME$SCRIPT_FILE"
 	if [ -f $SCRIPT_FILE_PATH ]; then
-		mysql --login-path=refocus_metadata_service_admin < $SCRIPT_FILE_PATH
+		mysql --login-path=refocus_metadata_service_admin refocus_metadata < $SCRIPT_FILE_PATH
 		if [ $? -ne 0 ]; then
 			echo "Error with script: $SCRIPT_FILE_PATH"
 			exit 1
