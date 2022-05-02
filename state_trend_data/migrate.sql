f3a38ca674e7655559c1e8e160aa3f741a1722f5
CThaw90 <christhaw90@gmail.com>
2022-05-02 12:59:02 -0400

    Updated migration script to reflect new positivity_rate_14_day_mean column in state_trends_data table

--- state_trend_data/migrate.sql
+++ state_trend_data/migrate.sql
@@ -8,7 +8,7 @@ INSERT INTO `refocus_metadata`.state_trend_data (
 SELECT
 	state, date, cases, deaths, tests, cases_change, deaths_change, tests_change, cases_7_day_mean,
 	deaths_7_day_mean, tests_7_day_mean, cases_per_million, deaths_per_million, tests_per_million,
-	positivity_rate_7_day_mean, positivity_rate_7_day_plus_mean, 
+	positivity_rate_7_day_mean, positivity_rate_14_day_mean, 
 	pct_change_weekly_cases_7, pct_change_weekly_cases_14,
 	pct_change_weekly_deaths_7, pct_change_weekly_deaths_14,
 	pct_change_weekly_tests_7, pct_change_weekly_tests_14,
