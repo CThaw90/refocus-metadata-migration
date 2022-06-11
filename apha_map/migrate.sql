76732962b03e90544f9628f163da87a6ea34cc61
CThaw90 <christhaw90@gmail.com>
2022-06-10 20:15:56 -0400

    Added longitude and latitude columns to apha_map data migration

--- apha_map/migrate.sql
+++ apha_map/migrate.sql
@@ -1,10 +1,10 @@
 INSERT INTO `refocus_metadata`.apha_map (
-	date, city, county, state, entity_type,
-	entity_geo, entity_name, link_to_declaration
+	date, city, county, state, entity_type, entity_geo, 
+	entity_name, link_to_declaration, latitude, longitude
 )
 SELECT
-	date, city, county, state, entity_type,
-	entity_geo, entity_name, link_to_declaration
+	date, city, county, state, entity_type, entity_geo,
+	entity_name, link_to_declaration, latitude, longitude
 FROM
 	`refocus`.apha_map
 ;
