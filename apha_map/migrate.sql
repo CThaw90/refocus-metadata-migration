1b30cf115136dadf5aa7dd9679c299999210d3e6
CThaw90 <christhaw90@gmail.com>
2022-06-13 21:33:40 -0400

    Added column declaration to the apha_map table migration

--- apha_map/migrate.sql
+++ apha_map/migrate.sql
@@ -1,10 +1,12 @@
 INSERT INTO `refocus_metadata`.apha_map (
-	date, city, county, state, entity_type, entity_geo, 
-	entity_name, link_to_declaration, latitude, longitude
+	date, city, county, state, entity_type,
+	entity_geo, entity_name, link_to_declaration,
+	latitude, longitude, declaration
 )
 SELECT
-	date, city, county, state, entity_type, entity_geo,
-	entity_name, link_to_declaration, latitude, longitude
+	date, city, county, state, entity_type,
+	entity_geo, entity_name, link_to_declaration,
+	latitude, longitude, declaration
 FROM
 	`refocus`.apha_map
 ;
